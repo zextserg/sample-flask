f61adafec28b13d8430865c433f90c26b0a5174f
jon doe <jon@doe.com>
2024-08-21 19:18:50 +0300

    edti filename

--- t_app.py
+++ t_app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask import render_template
 
 app = Flask(__name__)
@@ -7,3 +7,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template("index.html")
+
+@app.route("/test")
+def hello_test():
+    return jsonify({'status': 'test', 'data': [1,2,3]})
